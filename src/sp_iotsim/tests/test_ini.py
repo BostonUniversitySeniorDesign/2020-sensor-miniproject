0b8a414fe9eb80676447f7a85bfba46510a191e7
Michael Hirsch <scivision@users.noreply.github.com>
2020-09-09 11:43:12 -0400

    add generate test

--- src/sp_iotsim/tests/test_ini.py
+++ src/sp_iotsim/tests/test_ini.py
@@ -1,4 +1,5 @@
 import sp_iotsim.server as server
+import random
 
 
 def test_ini():
@@ -6,3 +7,12 @@ def test_ini():
     assert isinstance(rooms, dict)
 
     assert sorted(rooms["lab1"].keys()) == ["co", "loc", "occ", "scale"], "wrong keys"
+
+
+def test_generate():
+    rooms = server.get_simulated_rooms()
+    room = random.choice(list(rooms.keys()))
+
+    dat = server.generate_data(rooms[room])
+
+    assert sorted(dat.keys()) == ["co2", "occupancy", "temperature", "time"],  "wrong keys"
